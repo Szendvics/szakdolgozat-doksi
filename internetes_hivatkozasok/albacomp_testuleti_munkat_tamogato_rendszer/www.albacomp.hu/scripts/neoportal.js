3e0bec8694330db085dbca67a3db127864b0a3dd
Szendvics <eandr2@gmail.com>
2014-04-07 16:47:48 +0200

    reinit
    Signed-off-by: Szendvics <eandr2@gmail.com>

--- internetes_hivatkozasok/albacomp_testuleti_munkat_tamogato_rendszer/www.albacomp.hu/scripts/neoportal.js
+++
@@ -1,9 +0,0 @@
-function printerfriendly(link) 	{ 
-    window.open('print.php?pg='+link+'&nyelv=0','printpage','top=0,left=0,width=650,height=480,scrollbars=yes'); 
-}
-function printerfriendly_catalog(link) 	{ 
-    window.open('/print.php?'+link,'printpage','top=0,left=0,width=650,height=480,scrollbars=yes'); 
-}
-function download(moid) {
-	window.location='/download.php?moid='+moid; 
-}
